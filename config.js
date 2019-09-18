36c490d24de5ddd51bccd0bf666a0b896afb2ae2
kakisoft <kakisttab@gmail.com>
2019-09-18 23:48:26 +0900

    a

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   title: 'English TextBook 01', // Required
-  author: 'kakisoft', // Required
-  description: 'Web Developer',
+  author: 'English TextBook 01', // Required
+  // description: 'Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: false,
   showShareButtons: false,
   postsPerPage: 10, // Required
-  social: {
-    website: 'https://kakisoft-portfolio-v2.netlify.com/',
-    github: 'https://github.com/kakisoft',
-    twitter: 'https://twitter.com/kakisoft_tab',
-    linkedin: 'https://ca.linkedin.com/in/kakisoft',
-  },
+  // social: {
+  //   website: 'https://kakisoft-portfolio-v2.netlify.com/',
+  //   github: 'https://github.com/kakisoft',
+  //   twitter: 'https://twitter.com/kakisoft_tab',
+  //   linkedin: 'https://ca.linkedin.com/in/kakisoft',
+  // },
   pathPrefix: '/devblog',
   siteUrl: 'https://englishtextbook01.netlify.com/',
 };
